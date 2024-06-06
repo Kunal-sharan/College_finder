547d603e2abcfede150a279c05a9fa4c155ae542
Kunal sharan <117339459+Kunal-sharan@users.noreply.github.com>
2024-06-06 15:59:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -338,7 +338,8 @@ if but:
                 else:
                   for j in range(len(all_placements_data[k][i])):
                     st.dataframe(all_placements_data[k][i][j])
-        sorted_items=[]                      
+        sorted_items=[]
+        all_placements_data=[]
 llm = GoogleGenerativeAI(model="gemini-pro", google_api_key=st.secrets["GOOGLE_AI"])
 butt=st.button("Compare the two based on the placements data")
 if butt:
